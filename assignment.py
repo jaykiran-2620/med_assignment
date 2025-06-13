6b59affda9dabcf46452d321e4e27e0d40212bc0
jaykiran-2620 <jaykiran205@gmail.com>
2025-06-13 17:34:28 +0530

    questions

--- assignment.py
+++ assignment.py
@@ -19,5 +19,25 @@ print(fruits[2][0])
 
 #2
 
-coordinates = {(0, 1),(2, 3),(4, 5)}
+coordinates = ((0, 1),(2, 3),(4, 5))
 print(coordinates[2][1])
+
+
+
+#3
+
+students = [("Alice", 23), ("bob", 21)]
+print(students[1][1])
+
+
+
+#4
+
+lists = ([10, 20], [30, 40])
+print(lists[1][1])
+
+
+#5
+
+students = [{"name": "jay", "age": 23}, {"name": "john", "age": 24}, {"name": "alice", "age": 30}]
+print(students[0]["name"])
