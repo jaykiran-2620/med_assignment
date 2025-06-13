a9a7d621ff3ee5eb2aab70ff69929727e9595818
jaykiran-2620 <jaykiran205@gmail.com>
2025-06-13 17:36:56 +0530

    jay

--- assignment.py
+++ assignment.py
@@ -41,3 +41,111 @@ print(lists[1][1])
 
 students = [{"name": "jay", "age": 23}, {"name": "john", "age": 24}, {"name": "alice", "age": 30}]
 print(students[0]["name"])
+
+
+
+#6 
+
+
+ subject_marks={"math":[25,36,96],"science":[54,65,55],"english":[96,54,21]}
+ print(subject_marks["math"][1])
+
+
+
+#7
+
+
+ user_profile={
+     "alice":{
+         "name":"alice",
+         "age":25,
+         "skill":"python",
+         "language":"english"
+
+     }
+ }
+ print(user_profile["alice"])
+
+
+
+#8  
+
+
+
+ billing_history=[
+     {"name":"john","amount":250,"method":"cash","status":"paid"},
+     {"name":"alice","amount":1230,"method":"cash","status":"pending"},
+     {"name":"sunny","amount":654,"method":"upi","status":"paid"}
+ ]
+ print(billing_history[1]["status"])
+
+
+#9 
+
+
+ billing_history=(
+     {"name":"john","amount":250,"method":"cash","status":"paid"},
+     {"name":"alice","amount":1230,"method":"cash","status":"pending"},
+     {"name":"sunny","amount":654,"method":"upi","status":"paid"}
+ )
+ print(billing_history[1]["amount"])
+
+
+#10 
+
+
+ data=[("hyderabad",40),("banglore",42),("chennai",45)]
+ print(data[2][-1])
+
+
+#11
+
+
+
+ user_names={
+     "john":[25,65,98],
+     "don":[45,65,97],
+     "sun":[57,32,21]
+ }
+ print(user_names['sun'][-1])
+
+
+#12
+
+
+ price=[
+     {"product":"phone","price":12000},
+     {"product":"laptop","price":40000}
+ ]
+ print(price[1]["product"])
+
+
+
+#13
+
+
+ marks=[
+     ("maths",[65]),("science",[98]),("english",[54])
+ ]
+ print(marks[2][1][0])
+
+
+
+#14
+
+
+ task={
+     "monday":("reading","sleeping"),"tuesday":("watching","playing"),"wednesday":("writing","swiming"),
+     "thursday":("runing"),"friday":("walking"),"saturday":("gym","studying"),"sunday":("read","write")
+ }
+ print(task["monday"][0])
+
+
+#15 
+
+
+ data=[
+     {"jay":{"maths":65,"english":54,"science":32}},
+     {"john":{"math":31,"english":41,"science":78}}
+ ]
+ print(data[0]["jay"]["maths"])
