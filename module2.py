e2d11edf7af84270092964dae92b844ffb9b2101
jaykiran-2620 <jaykiran205@gmail.com>
2025-06-13 18:23:42 +0530

    conditions

---
+++ module2.py
@@ -0,0 +1,33 @@
+   #1 IF
+
+if True:
+    print("works")
+
+
+    #2 IF ELSE
+
+    if True:
+        print("one")
+    else:
+        print("two")
+
+
+
+#3  IF-ELIF-ELSE
+
+a, b = 1, 3
+if a > b:
+    print("one")
+elif a == b:
+    print("two")
+else:
+    print("done")
+
+
+    #4 IF-ELIF
+
+    a, b = 1, 3
+    if a<b:
+        print("one")
+    elif b>a:
+        print("two")
